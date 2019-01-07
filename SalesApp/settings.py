00835257f9a79ba056f85fd95f0c0fc9f03d0cae
tanishqandmac <tanishqandmac@gmail.com>
2019-01-07 14:04:11 +0530

    Update settings.py

--- SalesApp/settings.py
+++ SalesApp/settings.py
@@ -29,7 +29,7 @@ TEMPLATE_DIRS = (
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
